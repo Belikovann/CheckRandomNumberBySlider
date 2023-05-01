e14d5efd24161d9cf3c7793e34958882f5a2cdd2
@Annabel_ <hydrant94@gmail.com>
2023-05-01 19:34:32 +0300

    Initial Commit

---
+++ CheckRandomNumberBySlider/CheckRandomNumberBySliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckRandomNumberBySliderApp.swift
+//  CheckRandomNumberBySlider
+//
+//  Created by Аня Беликова on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckRandomNumberBySliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
